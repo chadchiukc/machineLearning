1f2a9b10ae7d301760701240990ef4633cb9c07f
chadchiu <chadchiu318@gmail.com>
2021-10-04 23:56:01 +0800

    updated

--- computer_vision/edge.py
+++ computer_vision/edge.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from PIL import Image # pillow package
 import numpy as np
 from scipy import ndimage
@@ -46,8 +47,7 @@ def sobel(arr):
     '''Apply sobel operator on arr and return the result.'''
     # TODO: Please complete this function.
     # your code here
-    vertical_filter = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
-    # vertical_filter = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    vertical_filter = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
     horizontal_filter = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
     Gx = ndimage.convolve(arr, vertical_filter)
     Gy = ndimage.convolve(arr, horizontal_filter)
@@ -61,12 +61,10 @@ def nonmax_suppress(G, Gx, Gy):
     assert G.shape == Gx.shape
     assert G.shape == Gy.shape
     # TODO: Please complete this function.
-    print(G.max())
-    print(G.min())
     theta = np.arctan2(Gy, Gx) * 180 / np.pi
     theta[theta < 0] += 180
     x, y = G.shape
-    suppressed_G = np.zeros((x, y), dtype=np.int32)
+    suppressed_G = np.zeros(G.shape)
 
     for i in range(1, x - 1):
         for j in range(1, y - 1):
@@ -74,19 +72,17 @@ def nonmax_suppress(G, Gx, Gy):
                 N1 = G[i, j + 1]
                 N2 = G[i, j - 1]
             elif 22.5 <= theta[i, j] < 67.5:
-                N1 = G[i + 1, j - 1]
-                N2 = G[i - 1, j + 1]
+                N1 = G[i - 1, j - 1]
+                N2 = G[i + 1, j + 1]
             elif 67.5 <= theta[i, j] < 112.5:
                 N1 = G[i + 1, j]
                 N2 = G[i - 1, j]
-            elif 112.5 <= theta[i, j] < 157.5:
-                N1 = G[i - 1, j - 1]
-                N2 = G[i + 1, j + 1]
+            else:
+                N1 = G[i + 1, j - 1]
+                N2 = G[i - 1, j + 1]
 
             if (G[i, j] >= N1) and (G[i, j] >= N2):
                 suppressed_G[i, j] = G[i, j]
-            else:
-                suppressed_G[i, j] = 0
     return suppressed_G
 
 def thresholding(G, low, high):
@@ -96,95 +92,55 @@ def thresholding(G, low, high):
     assert high <= 361
     assert low >= 0 & low < high
     x, y = G.shape
-    result = np.zeros((x, y), dtype=np.int32)
-    weak = 100
-    strong = 360
+    result = np.zeros(G.shape)
+    weak = 10
+    strong = 255
+    G_low = G.copy()
+    G_high = G.copy()
+    G_low[G_low < low] = 0
+    G_low[G_low >= high] = 0
+    G_low[G_low > 0] = 255
+    G_high[G_high < high] = 0
+    G_high[G_high > 0] = 255
+    # show_array_as_img(G_low)
+    save_array_as_img(G_low, 'edgemap_low.jpg')
+    # show_array_as_img(G_high)
+    save_array_as_img(G_high, 'edgemap_high.jpg')
 
     strong_i, strong_j = np.where(G >= high)
-    weak_i, weak_j = np.where((G <= high) & (G >= low))
-
+    weak_i, weak_j = np.where((G < high) & (G >= low))
     result[strong_i, strong_j] = strong
     result[weak_i, weak_j] = weak
-
-    # for i in range(1, x-1):
-    #     for j in range(1, y-1):
-    #         if result[i,j] == weak:
-    #             if (result[i+1, j-1] == strong or result[i+1, j] == strong or result[i+1, j+1] == strong
-    #                 or result[i, j-1] == strong or result[i, j+1] == strong
-    #                 or result[i-1, j-1] == strong or result[i-1, j] == strong or result[i-1, j+1] == strong):
-    #                 result[i, j] = strong
-    #             else:
-    #                 result[i, j] = 0
-    # G = result
-
-    top_to_bottom = result.copy()
-    bottom_to_top = result.copy()
-    right_to_left = result.copy()
-    left_to_right = result.copy()
-    image_row, image_col = result.shape
-    for row in range(1, image_row):
-        for col in range(1, image_col):
-            if top_to_bottom[row, col] == weak:
-                if top_to_bottom[row, col + 1] == strong or top_to_bottom[row, col - 1] == strong or top_to_bottom[
-                    row - 1, col] == strong or top_to_bottom[
-                    row + 1, col] == strong or top_to_bottom[
-                    row - 1, col - 1] == strong or top_to_bottom[row + 1, col - 1] == strong or top_to_bottom[
-                    row - 1, col + 1] == strong or top_to_bottom[
-                    row + 1, col + 1] == strong:
-                    top_to_bottom[row, col] = strong
+    for i in range(1, x-1):
+        for j in range(1, y-1):
+            if result[i,j] == weak:
+                if (result[i+1, j-1] == strong or result[i+1, j] == strong or result[i+1, j+1] == strong
+                    or result[i, j-1] == strong or result[i, j+1] == strong or result[i-1, j-1] == strong
+                        or result[i-1, j] == strong or result[i-1, j+1] == strong):
+                    result[i, j] = strong
                 else:
-                    top_to_bottom[row, col] = 0
-
-
-    for row in range(image_row - 1, 0, -1):
-        for col in range(image_col - 1, 0, -1):
-            if bottom_to_top[row, col] == weak:
-                if bottom_to_top[row, col + 1] == strong or bottom_to_top[row, col - 1] == strong or bottom_to_top[
-                    row - 1, col] == strong or bottom_to_top[
-                    row + 1, col] == strong or bottom_to_top[
-                    row - 1, col - 1] == strong or bottom_to_top[row + 1, col - 1] == strong or bottom_to_top[
-                    row - 1, col + 1] == strong or bottom_to_top[
-                    row + 1, col + 1] == strong:
-                    bottom_to_top[row, col] = strong
-                else:
-                    bottom_to_top[row, col] = 0
-
-
-    for row in range(1, image_row):
-        for col in range(image_col - 1, 0, -1):
-            if right_to_left[row, col] == weak:
-                if right_to_left[row, col + 1] == strong or right_to_left[row, col - 1] == strong or right_to_left[
-                    row - 1, col] == strong or right_to_left[
-                    row + 1, col] == strong or right_to_left[
-                    row - 1, col - 1] == strong or right_to_left[row + 1, col - 1] == strong or right_to_left[
-                    row - 1, col + 1] == strong or right_to_left[
-                    row + 1, col + 1] == strong:
-                    right_to_left[row, col] = strong
-                else:
-                    right_to_left[row, col] = 0
-
-    for row in range(image_row - 1, 0, -1):
-        for col in range(1, image_col):
-            if left_to_right[row, col] == weak:
-                if left_to_right[row, col + 1] == strong or left_to_right[row, col - 1] == strong or left_to_right[
-                    row - 1, col] == strong or left_to_right[
-                    row + 1, col] == strong or left_to_right[
-                    row - 1, col - 1] == strong or left_to_right[row + 1, col - 1] == strong or left_to_right[
-                    row - 1, col + 1] == strong or left_to_right[
-                    row + 1, col + 1] == strong:
-                    left_to_right[row, col] = strong
-                else:
-                    left_to_right[row, col] = 0
-    final_image = top_to_bottom + bottom_to_top + right_to_left + left_to_right
-
-    final_image[final_image > 255] = 255
-    return final_image
+                    result[i, j] = 0
+    G = result
+    return G
 
 def hough(G):
     '''Return Hough transform of G'''
     # TODO: Please complete this function.
     # your code here
-    pass
+    steps = 10
+    x, y = G.shape
+    D = int(np.sqrt(np.square(x) + np.square(y)))
+    thetas = np.deg2rad(np.arange(0, 180, step=180/steps))
+    rhos = np.linspace(-D, D, 2*D)
+    accumulator = np.zeros((len(rhos), len(thetas)))
+    for i in range(x):
+        for j in range(y):
+            if G[i, j] == 255:
+                for k in range(len(thetas)):
+                    rho = j * np.cos(thetas[k]) + i * np.sin(thetas[k])
+                    accumulator[int(rho) + D, k] += 1
+    return accumulator, thetas, rhos
+
 
 img = read_img_as_array('road.jpeg')
 #TODO: detect edges on 'img'
@@ -194,14 +150,14 @@ gray_img = rgb2gray(img)
 save_array_as_img(gray_img, 'gray.jpg')
 
 # question 2
-gauss_img = ndimage.gaussian_filter(gray_img, sigma=1.3)
+gauss_img = ndimage.gaussian_filter(gray_img, sigma=1.2)
 # show_array_as_img(gauss_img)
 save_array_as_img(gauss_img, 'gauss.jpg')
 
 # question 3
 G, Gx, Gy = sobel(gauss_img)
-show_array_as_img(G)
-show_array_as_img(Gx)
+# show_array_as_img(G)
+# show_array_as_img(Gx)
 # show_array_as_img(Gy)
 save_array_as_img(G, 'G.jpg')
 save_array_as_img(Gx, 'G_x.jpg')
@@ -213,6 +169,13 @@ nonmax_suppress_img = nonmax_suppress(G,Gx,Gy)
 save_array_as_img(nonmax_suppress_img, 'supress.jpg')
 
 # question 5
-thresholding_img = thresholding(nonmax_suppress_img, 10, 30)
-show_array_as_img(thresholding_img)
-
+thresholding_img = thresholding(nonmax_suppress_img, 20, 40)
+# show_array_as_img(thresholding_img)
+save_array_as_img(thresholding_img, 'edgemap.jpg')
+accumulator, thetas, rhos = hough(thresholding_img)
+show_array_as_img(accumulator)
+# idx = np.argmax(accumulator)
+# rho = int(rhos[int(idx / accumulator.shape[1])])
+# theta = thetas[int(idx % accumulator.shape[1])]
+# print("rho={0:.0f}, theta={1:.0f}".format(rho, np.rad2deg(theta)))
+print(accumulator.shape)
